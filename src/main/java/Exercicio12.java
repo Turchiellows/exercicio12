af403a47d97c4cbaf11df0a567a971e15fe435b5
root <root@DELL-7559>
2024-09-18 20:43:26 -0300

    Início do projeto

---
+++ src/main/java/Exercicio12.java
@@ -0,0 +1,31 @@
+// IMPORTS
+
+import java.util.Scanner;
+
+public class Exercicio12 {
+
+	public static void main(String[] args) {
+		// INSTANTIATE
+		Scanner sc = new Scanner(System.in);
+
+		// DECLARATE
+		int a;
+		int b;
+		int result;
+
+		// COLLECT DATA
+		System.out.print("A : ");
+		a = sc.nextInt();
+		System.out.print("B : ");
+		b = sc.nextInt();
+
+		// OUTPUT
+		if (a > b) {
+			System.out.println(a);
+		} else if (a == b) {
+			System.out.println("EQUALS");
+		} else {
+			System.out.println(b);
+		}
+	}
+}

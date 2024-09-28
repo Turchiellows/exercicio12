221f24cee4b628216358cd2a559ecaa16be5f848
Turchiellows <>
2024-09-28 13:55:51 -0300

    refactor: Mudança de indentação

--- src/main/java/Exercicio12.java
+++ src/main/java/Exercicio12.java
@@ -4,28 +4,28 @@ import java.util.Scanner;
 
 public class Exercicio12 {
 
-	public static void main(String[] args) {
-		// INSTANTIATE
-		Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      // INSTANTIATE
+      Scanner sc = new Scanner(System.in);
 
-		// DECLARATE
-		int a;
-		int b;
-		int result;
+      // DECLARATE
+      int a;
+      int b;
+      int result;
 
-		// COLLECT DATA
-		System.out.print("A : ");
-		a = sc.nextInt();
-		System.out.print("B : ");
-		b = sc.nextInt();
+      // COLLECT DATA
+      System.out.print("A : ");
+      a = sc.nextInt();
+      System.out.print("B : ");
+      b = sc.nextInt();
 
-		// OUTPUT
-		if (a > b) {
-			System.out.println(a);
-		} else if (a == b) {
-			System.out.println("EQUALS");
-		} else {
-			System.out.println(b);
-		}
-	}
+      // OUTPUT
+      if (a > b) {
+         System.out.println(a);
+      } else if (a == b) {
+         System.out.println("EQUALS");
+      } else {
+         System.out.println(b);
+      }
+   }
 }
